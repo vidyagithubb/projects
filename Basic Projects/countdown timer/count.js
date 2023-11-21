4c57e0b21ecd1b1c051571a555429688dfa44038
vidyagithubb <102215277+vidyagithubb@users.noreply.github.com>
2023-11-21 16:12:01 +0530

    Add files via upload

---
+++ Basic Projects/countdown timer/count.js
@@ -0,0 +1,59 @@
+// Getting formated date from date string
+let deadline = new Date(
+    "dec 31, 2023 15:37:25"
+).getTime();
+
+// Calling defined function at certain interval
+let x = setInterval(function () {
+
+    // Getting current date and time in required format
+    let now = new Date().getTime();
+
+    // Calculating difference
+    let t = deadline - now;
+
+    // Getting values of days,hours,minutes, seconds
+    let days = Math.floor(
+        t / (1000 * 60 * 60 * 24)
+    );
+    let hours = Math.floor(
+        (t % (1000 * 60 * 60 * 24)) /
+            (1000 * 60 * 60)
+    );
+    let minutes = Math.floor(
+        (t % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let seconds = Math.floor(
+        (t % (1000 * 60)) / 1000
+    );
+
+    // Show the output time
+    document.getElementById("day")
+            .innerHTML = days;
+    document.getElementById("hour")
+            .innerHTML = hours;
+    document.getElementById("minute")
+            .innerHTML = minutes;
+    document.getElementById("second")
+            .innerHTML = seconds;
+
+    // Show overtime output
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById(
+            "demo"
+        ).innerHTML = "TIME UP";
+        document.getElementById(
+            "day"
+        ).innerHTML = "0";
+        document.getElementById(
+            "hour"
+        ).innerHTML = "0";
+        document.getElementById(
+            "minute"
+        ).innerHTML = "0";
+        document.getElementById(
+            "second"
+        ).innerHTML = "0";
+    }
+}, 1000);
